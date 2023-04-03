9a40d3f8f8994b51dbf16bd25a1392ef1d4bfcb6
Zeynep Akbalık <zeynep@leodrive.ai>
2023-04-03 16:12:03 +0300

    camera container created

--- launch/tier4_perception_launch/launch/object_recognition/detection/camera_container.launch.py
+++ launch/tier4_perception_launch/launch/object_recognition/detection/camera_container.launch.py
@@ -1,22 +1,18 @@
-# Copyright 2020 Tier IV, Inc. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# # Copyright 2020 Tier IV, Inc. All rights reserved.
+# #
+# # Licensed under the Apache License, Version 2.0 (the "License");
+# # you may not use this file except in compliance with the License.
+# # You may obtain a copy of the License at
+# #
+# #     http://www.apache.org/licenses/LICENSE-2.0
+# #
+# # Unless required by applicable law or agreed to in writing, software
+# # distributed under the License is distributed on an "AS IS" BASIS,
+# # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# # See the License for the specific language governing permissions and
+# # limitations under the License.
 
-import os
-
-from ament_index_python.packages import get_package_share_directory
 import launch
-from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.actions import SetLaunchConfiguration
 from launch.conditions import IfCondition
@@ -28,60 +24,29 @@ from launch_ros.actions import LoadComposableNodes
 from launch_ros.descriptions import ComposableNode
 from launch_ros.substitutions import FindPackageShare
 from launch.substitutions import PathJoinSubstitution
+from launch.actions import OpaqueFunction
 
 from launch import LaunchContext
-
 import yaml
 
-def generate_launch_description():
-    launch_arguments = []
-    context = LaunchContext()
-
-    use_multithread = "True"
+def launch_setup(context, *args, **kwargs):
     use_intra_process = "True"
     output_topic= "rois0"
 
-    def create_parameter_dict(*args):
-        result = {}
-        for x in args:
-            result[x] = LaunchConfiguration(x)
-        return result
+    input_image = LaunchConfiguration("input_image").perform(context)
+    input_camera_info= LaunchConfiguration("input_camera_info").perform(context)
 
-    def add_launch_arg(name: str, default_value=None, description=None):
-        # a default_value of None is equivalent to not passing that kwarg at all
-        launch_arguments.append(
-            DeclareLaunchArgument(name, default_value=default_value, description=description)
-        )
 
-    # tensorrt params
-    add_launch_arg("yolo_type", description="yolo model type")
-    add_launch_arg("input_image", description="input camera topic")
-    add_launch_arg("input_camera_info", description="input camera info topic")
-    add_launch_arg("label_file", description="tensorrt node label file")
-    add_launch_arg("gpu_id", description="gpu setting")
-    add_launch_arg("mode", description="the inference mode:FP32,FP16,INT8")
-    add_launch_arg("camera_param_path", description="camera parameter file path")
-
-    camera_node_params=create_parameter_dict(
-            "mode",
-            "gpu_id",
-            "yolo_type",
-            "label_file",
-            "input_image",
-            "input_camera_info",
-            "camera_param_path",
-
-        )
-
+    gpu_id = int(LaunchConfiguration("gpu_id").perform(context))
+    mode= LaunchConfiguration("mode").perform(context)
     calib_image_directory= FindPackageShare("tensorrt_yolo").perform(context) + "/calib_image/"
-    tensorrt_config_path = FindPackageShare('tensorrt_yolo').perform(context)+ "/config/" + str(camera_node_params["yolo_type"]) + ".param.yaml"
-    #
-    with open(tensorrt_config_path, "r") as f:
-        tensorrt_yaml_param = yaml.safe_load(f)["/**"]["ros__parameters"]
-    print(222222)
-    # camera_param_path = "/home/leodrive/projects/autoware/src/sensor_component/external/arena_camera/param/b_camera.param.yaml"
+    tensorrt_config_path = FindPackageShare('tensorrt_yolo').perform(context)+ "/config/" + LaunchConfiguration("yolo_type").perform(context) + ".param.yaml"
 
-    with open(camera_node_params["camera_param_path"], "r") as f:
+    with open(tensorrt_config_path, "r") as f:
+            tensorrt_yaml_param = yaml.safe_load(f)["/**"]["ros__parameters"]
+    camera_param_path =LaunchConfiguration("camera_param_file").perform(context)
+
+    with open(camera_param_path, "r") as f:
         camera_yaml_param = yaml.safe_load(f)["/**"]["ros__parameters"]
 
 
@@ -90,6 +55,7 @@ def generate_launch_description():
         namespace="/perception/object_detection",
         package="rclcpp_components",
         executable=LaunchConfiguration("container_executable"),
+        output="screen",
         composable_node_descriptions=[
             ComposableNode(
                 package="arena_camera",
@@ -110,6 +76,8 @@ def generate_launch_description():
                     "gain_auto": camera_yaml_param['gain_auto'],
                     "gain_target": camera_yaml_param['gain_target'],
                     "gamma_target": camera_yaml_param['gamma_target'],
+                    "enable_compressing": camera_yaml_param['enable_compressing'],
+                    "enable_rectifying": camera_yaml_param['enable_rectifying'],
                 }],
                 remappings=[
                 ],
@@ -123,8 +91,8 @@ def generate_launch_description():
                 name='rectify_front_camera_image_node',
                 # Remap subscribers and publishers
                 remappings=[
-                    ('image', camera_node_params["input_image"]),
-                    ('camera_info', camera_node_params["input_camera_info"]),
+                    ('image', input_image),
+                    ('camera_info', input_camera_info),
                     ('image_rect', 'image_rect_front')
                 ],
                 extra_arguments=[
@@ -136,24 +104,23 @@ def generate_launch_description():
                 plugin="object_recognition::TensorrtYoloNodelet",
                 name="front_camera_tensorrt_yolo",
                 parameters=[
-
                     {
-                        "gpu_id": camera_node_params["gpu_id"],
-                        "mode": camera_node_params["mode"],
-                        "onnx_file": FindPackageShare("tensorrt_yolo").perform(context) +  "/data/" + str(camera_node_params["yolo_type"]) + ".onnx",
-                        "engine_file":  FindPackageShare("tensorrt_yolo").perform(context) + "/data/"+ str(camera_node_params["yolo_type"]) + ".engine",
-                        "label_file": FindPackageShare("tensorrt_yolo").perform(context) + "/data/" + str(camera_node_params["label_file"]),
-                        "calib_image_directory": calib_image_directory,
-                        "calib_cache_file": FindPackageShare("tensorrt_yolo").perform(context) + "/data/" + camera_node_params["yolo_type"] + ".cache",
-                        "num_anchors": tensorrt_yaml_param['num_anchors'],
-                        "anchors": tensorrt_yaml_param['anchors'],
-                        "scale_x_y": tensorrt_yaml_param['scale_x_y'],
-                        "score_threshold": tensorrt_yaml_param['score_threshold'],
-                        "iou_thresh": tensorrt_yaml_param['iou_thresh'],
-                        "detections_per_im": tensorrt_yaml_param['detections_per_im'],
-                        "use_darknet_layer": tensorrt_yaml_param['use_darknet_layer'],
-                        "ignore_thresh": tensorrt_yaml_param['ignore_thresh'],
-                    }
+                    "mode": mode,
+                    "gpu_id": gpu_id,
+                    "onnx_file": FindPackageShare("tensorrt_yolo").perform(context) +  "/data/" + LaunchConfiguration("yolo_type").perform(context) + ".onnx",
+                    "label_file": FindPackageShare("tensorrt_yolo").perform(context) + "/data/" + LaunchConfiguration("label_file").perform(context),
+                    "engine_file": FindPackageShare("tensorrt_yolo").perform(context) + "/data/"+ LaunchConfiguration("yolo_type").perform(context) + ".engine",
+                    "calib_image_directory": calib_image_directory,
+                    "calib_cache_file": FindPackageShare("tensorrt_yolo").perform(context) + "/data/" + LaunchConfiguration("yolo_type").perform(context) + ".cache",
+                    "num_anchors": tensorrt_yaml_param['num_anchors'],
+                    "anchors": tensorrt_yaml_param['anchors'],
+                    "scale_x_y": tensorrt_yaml_param['scale_x_y'],
+                    "score_threshold": tensorrt_yaml_param['score_threshold'],
+                    "iou_thresh": tensorrt_yaml_param['iou_thresh'],
+                    "detections_per_im": tensorrt_yaml_param['detections_per_im'],
+                    "use_darknet_layer": tensorrt_yaml_param['use_darknet_layer'],
+                    "ignore_thresh": tensorrt_yaml_param['ignore_thresh'],
+                }
                 ],
                 remappings=[
                     ("in/image", 'image_rect_front'),
@@ -165,26 +132,47 @@ def generate_launch_description():
                 ],
             ),
         ],
-        output="both",
+
     )
+    return [container]
+
+
+def generate_launch_description():
+    launch_arguments = []
+    context = LaunchContext()
+
+    def add_launch_arg(name: str, default_value=None, description=None):
+    # a default_value of None is equivalent to not passing that kwarg at all
+        launch_arguments.append(
+            DeclareLaunchArgument(name, default_value=default_value, description=description)
+    )
+    add_launch_arg("mode","")
+    add_launch_arg("input_image","", description="input camera topic")
+    add_launch_arg("input_camera_info", "", description="input camera info topic")
+    add_launch_arg("yolo_type","", description="yolo model type")
+    add_launch_arg("label_file","" ,description="tensorrt node label file")
+    add_launch_arg("gpu_id","", description="gpu setting")
+    add_launch_arg("camera_param_file","", description="camera parameter file path")
+    add_launch_arg("use_multithread", "True", "use multithread")
+
+
+
 
     set_container_executable = SetLaunchConfiguration(
         "container_executable",
         "component_container",
-        condition=UnlessCondition(use_multithread),
+        condition=UnlessCondition(LaunchConfiguration("use_multithread")),
     )
 
     set_container_mt_executable = SetLaunchConfiguration(
         "container_executable",
         "component_container_mt",
-        condition=IfCondition(use_multithread),
+        condition=IfCondition(LaunchConfiguration("use_multithread")),
     )
 
-    return LaunchDescription(
-        [
-            *launch_arguments,
-            set_container_executable,
-            set_container_mt_executable,
-            container,
-        ]
+    return launch.LaunchDescription(
+        launch_arguments
+        + [set_container_executable, set_container_mt_executable]
+        + [OpaqueFunction(function=launch_setup)]
     )
+
